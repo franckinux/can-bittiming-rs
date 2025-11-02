f0e35bc290f8d9ce9976974ed92e462700878e3d
franckinux <contact@franck-barbenoire.fr>
2025-11-02 19:52:52 +0100

    use derive form of clap

--- src/main.rs
+++ src/main.rs
@@ -2,84 +2,63 @@ mod bxcan;
 
 use std::process;
 
-use clap::{Arg, ArgAction, command};
+use clap::{Arg, ArgAction, command, error, Parser, value_parser, ValueEnum};
 use serde_json;
 
 use bxcan::BxcanTiming;
 
 
+#[derive(Copy, Clone, Debug, ValueEnum)]
+enum Device {
+    Bxcan,
+}
+
+
+#[derive(Copy, Clone, Debug, ValueEnum)]
+enum OutputFormat {
+    Json,
+}
+
+
+#[derive(Parser)]
+struct Args {
+    #[arg(short, long, value_enum, help="The device the timings can will computed for")]
+    device: Device,
+    #[arg(short, long, help="Frequency at the entry of the prescaler (Hz)")]
+    frequency: u32,
+    #[arg(short, long, help="Bits per second")]
+    baudrate: Option<u32>,
+    #[arg(short, long, help="Sample point position (%)")]
+    sample_point_position: f64,
+    #[arg(short, long, value_enum, help="Output format")]
+    output_format: Option<OutputFormat>,
+    #[arg(short='w', long, default_value_t=1, help="Sync jump width")]
+    sjw: u32,
+}
+
+
 fn main() {
-    let matches = command!()
-        .arg(
-            Arg::new("device")
-            .short('d')
-            .long("device")
-            .action(ArgAction::Set)
-            .required(true)
-            .help("The devices the timings can be computed ['bxcan']")
-		)
-        .arg(
-            Arg::new("frequency")
-            .short('f')
-            .long("frequency")
-            .action(ArgAction::Set)
-            .value_parser(clap::value_parser!(u32))
-            .required(true)
-            .help("Frequency at the entry of the prescaler (Hz)")
-		)
-        .arg(
-            Arg::new("baudrate")
-            .short('b')
-            .long("baudrate")
-            .action(ArgAction::Set)
-            .value_parser(clap::value_parser!(u32))
-            .help("Bits per second")
-		)
-        .arg(
-            Arg::new("sample-point-position")
-            .short('s')
-            .long("sample-point")
-            .action(ArgAction::Set)
-            .value_parser(clap::value_parser!(f64))
-            .required(true)
-            .help("Sample point position (%)")
-        )
-        .arg(
-            Arg::new("output-format")
-            .short('o')
-            .long("output-format")
-            .action(ArgAction::Set)
-            .help("Output format ['json']")
-		)
-        .arg(
-            Arg::new("sjw")
-            .short('j')
-            .long("sjw")
-            .action(ArgAction::Set)
-            .value_parser(clap::value_parser!(u32))
-            .default_value("1")
-            .help("Sync jump width (default: 1)")
-        ).arg_required_else_help(true)
-        .get_matches();
+    let args = Args::parse();
+
 
     // required arguments
-    let device = matches.get_one::<String>("device").unwrap();
-    let frequency = *matches.get_one::<u32>("frequency").unwrap();
-    let sample_point_position = *matches.get_one::<f64>("sample-point-position").unwrap();
+    let device = args.device;
+    let frequency = args.frequency;
+    let sample_point_position = args.sample_point_position;
 
     // optional arguments
-    let baud_rate = matches.get_one::<u32>("baudrate");
-    let format = matches.get_one::<String>("output-format");
+    let baud_rate = args.baudrate;
+    let output_format = args.output_format;
 
     // optional arguments with default value
-    let sjw = *matches.get_one::<u32>("sjw").unwrap();
+    let sjw = args.sjw;
 
     let mut baud_rates = vec!(
         1_000_000, 500_000, 250_000, 125_000, 100_000, 83_333, 50_000, 20_000, 10_000
     );
     if let Some(baud_rate) = baud_rate {
         if baud_rates.contains(&baud_rate) {
-            baud_rates = vec!(*baud_rate);
+            baud_rates = vec!(baud_rate);
         } else {
             eprintln!("Valid baudrates are {:?}", baud_rates);
             process::exit(1);
@@ -89,29 +68,22 @@ fn main() {
     if sample_point_position < 50.0 || sample_point_position > 90.0 {
         eprintln!("Sample point position must be in the interval [50.0 - 90.0]");
         process::exit(1);
+        // error("Sample point position must be in the interval [50.0 - 90.0]");
     }
     let spp = sample_point_position / 100.0;
 
     let mut results = vec!();
-    match device.as_str() {
-        "bxcan" => {
+    match device {
+        Device::Bxcan => {
             for baud_rate in baud_rates {
                 results.append(&mut BxcanTiming::timings(frequency, baud_rate, spp, sjw));
             };
         },
-        other => {
-            eprintln!("Device not implemented ({})", other);
-            process::exit(1);
-        },
     };
 
-    match format {
-        Some(format) => match format.as_str() {
-            "json" => println!("{}", serde_json::to_string_pretty(&results).unwrap()),
-            _ => {
-                eprintln!("Unknown format");
-                process::exit(1);
-            },
+    match output_format {
+        Some(OutputFormat::Json) => {
+            println!("{}", serde_json::to_string_pretty(&results).unwrap());
         },
         None => for r in &results {
             println!("{}", r);
